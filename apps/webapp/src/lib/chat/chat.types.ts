14571d838089b9184cec99e949a0f6d0fdf0f35b
Ore Adeleye <oreadeleye67@gmail.com>
2025-02-27 18:06:06 +0100

    Remove interdependence of repos

--- apps/webapp/src/lib/chat/chat.types.ts
+++ apps/webapp/src/lib/chat/chat.types.ts
@@ -19,11 +19,9 @@ export type Message = {
   };
 };
 
-export declare enum ChatEvents {
+export enum ChatEvents {
   JOIN_ROOM = "joinRoom",
   SEND_MESSAGE = "sendMessage",
   LEAVE_ROOM = "leaveRoom",
-  ERROR = "error",
   MESSAGE = "message",
-  ACTIVE_USERS = "activeUsers",
 }
